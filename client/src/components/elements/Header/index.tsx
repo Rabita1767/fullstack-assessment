6cdee668472ca05210dd2603a532a420256ebeeb
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2024-04-12 14:20:55 +0600

    Adjusted Edit product page

--- client/src/components/elements/Header/index.tsx
+++ client/src/components/elements/Header/index.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@mantine/core";
 import "./index.scss";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
+      <div>
+        <Link to={"/products/create"}>
+          <Button>CREATE PRODUCT</Button>
+        </Link>
+      </div>
       <Button>LOG OUT</Button>
     </div>
   );
--- client/src/components/pages/EditProduct/index.tsx
+++ client/src/components/pages/EditProduct/index.tsx
@@ -1,8 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import Layout from "../../layouts/Layout";
-import { Input, InputWrapper, MultiSelect, Textarea } from "@mantine/core";
+import {
+  Button,
+  Input,
+  InputWrapper,
+  MultiSelect,
+  Textarea,
+} from "@mantine/core";
 import { useParams } from "react-router-dom";
-import { gql, useQuery } from "@apollo/client";
+import { gql, useLazyQuery, useMutation, useQuery } from "@apollo/client";
+import "./index.scss";
+import { notifications } from "@mantine/notifications";
 
 const EditProduct = () => {
   const { id } = useParams();
@@ -11,17 +19,13 @@ const EditProduct = () => {
     description: string;
     price: string;
     rent: string;
-    // posted: string;
-    // views: string;
-    // status: string;
+    category: string[];
   }>({
     title: "",
     description: "",
     price: "",
     rent: "",
-    // posted: "",
-    // views: "",
-    // status: "",
+    category: [],
   });
   const SINGLE_PRODUCT_QUERY = gql`
     query Query($id: String!) {
@@ -37,12 +41,14 @@ const EditProduct = () => {
         category_product {
           id
           category {
+            id
             name
           }
         }
       }
     }
   `;
+
   const CATEGORY_LIST_QUERY = gql`
     query Query {
       category {
@@ -51,10 +57,48 @@ const EditProduct = () => {
       }
     }
   `;
+
+  const UPDATE_PRODUCT_QUERY = gql`
+    mutation Mutation(
+      $id: ID
+      $title: String
+      $description: String
+      $price: Int
+      $category: [String]
+      $rent: Int
+    ) {
+      productUpdate(
+        id: $id
+        title: $title
+        description: $description
+        price: $price
+        category: $category
+        rent: $rent
+      ) {
+        id
+        title
+        price
+        price
+        rent
+        posted
+        views
+        category_product {
+          id
+          category {
+            name
+          }
+        }
+      }
+    }
+  `;
+
   const { data: productDetails } = useQuery(SINGLE_PRODUCT_QUERY, {
     variables: { id: id },
   });
   const { data: categoryList } = useQuery(CATEGORY_LIST_QUERY);
+  const [triggerUpdateProduct, { data: updatedProduct }] =
+    useMutation(UPDATE_PRODUCT_QUERY);
+
   const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
 
   useEffect(() => {
@@ -64,53 +108,121 @@ const EditProduct = () => {
         description: productDetails?.product.description as string,
         price: productDetails?.product.price as string,
         rent: productDetails?.product.rent as string,
-        // posted: "",
-        // views: "",
-        // status: ""
+        category: productDetails?.product.category_product.map(
+          (element) => element.category.id
+        ),
       });
-      setSelectedCategories(
-        productDetails?.product.category_product.map((element) => element["id"])
-      );
+      // setSelectedCategories(
+      //   productDetails?.product.category_product.map((element) => element["id"])
+      // );
     }
   }, [productDetails]);
-  // console.log(selectedCategories);
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const requestBody: { [key: string]: string | number | string[] } = {};
+
+    if (productData.title !== productDetails.product.title) {
+      requestBody["title"] = productData.title;
+    }
+
+    if (productData.description !== productDetails.product.description) {
+      requestBody["description"] = productData.description;
+    }
+
+    if (productData.price !== productDetails.product.price) {
+      requestBody["price"] = Number(productData.price);
+    }
+
+    if (productData.rent !== productDetails.product.rent) {
+      requestBody.rent = Number(productData.rent);
+    }
+
+    if (Object.keys(requestBody).length === 0) {
+      notifications.show({
+        title: "Error",
+        message: "Nothing to update!",
+        color: "red",
+      });
+    }
+
+    requestBody["category"] = productData.category;
+    if (id) {
+      requestBody["id"] = id;
+    }
+
+    triggerUpdateProduct({ variables: requestBody });
+  };
+
+  console.log(categoryList);
+
   return (
     <Layout>
-      <InputWrapper label="Title">
-        <Input
-          value={productData.title}
-          onChange={(e) =>
-            setProductData((prevState) => ({
-              ...prevState,
-              title: e.target.value,
-            }))
-          }
-        />
-      </InputWrapper>
-      <InputWrapper label="Category">
-        <MultiSelect
-          value={selectedCategories}
-          onChange={(e) => {
-            // console.log(e);
-            setSelectedCategories(e);
-          }}
-          data={categoryList?.category.map((element) => ({
-            label: element.name,
-            value: element.id,
-          }))}
-        />
-      </InputWrapper>
-      <InputWrapper label="Description">
-        <Textarea
-          value={productData.description}
-          onChange={(e) =>
-            setProductData((prevState) => ({
-              ...prevState,
-              description: e.target.value,
-            }))
-          }
-        />
-      </InputWrapper>
+      <form className="editproduct" onSubmit={onSubmit}>
+        <InputWrapper label="Title" className="editproduct_title">
+          <Input
+            value={productData.title}
+            onChange={(e) =>
+              setProductData((prevState) => ({
+                ...prevState,
+                title: e.target.value,
+              }))
+            }
+          />
+        </InputWrapper>
+        <InputWrapper label="Category" className="editproduct_category">
+          <MultiSelect
+            value={productData.category}
+            onChange={(e) => {
+              // console.log(e);
+              setProductData((prevState) => ({ ...prevState, category: e }));
+            }}
+            data={categoryList?.category.map((element) => ({
+              label: element.name,
+              value: element.id,
+            }))}
+          />
+        </InputWrapper>
+        <InputWrapper label="Description" className="editproduct_description">
+          <Textarea
+            // minRows={50}
+            autosize
+            resize="vertical"
+            value={productData.description}
+            onChange={(e) =>
+              setProductData((prevState) => ({
+                ...prevState,
+                description: e.target.value,
+              }))
+            }
+          />
+        </InputWrapper>
+        <InputWrapper label="Price" className="editproduct_price">
+          <Input
+            value={productData.price}
+            onChange={(e) =>
+              setProductData((prevState) => ({
+                ...prevState,
+                price: e.target.value,
+              }))
+            }
+          />
+        </InputWrapper>
+        <InputWrapper label="Rent" className="editproduct_rent">
+          <Input
+            value={productData.rent}
+            onChange={(e) =>
+              setProductData((prevState) => ({
+                ...prevState,
+                rent: e.target.value,
+              }))
+            }
+          />
+        </InputWrapper>
+        <div className="editproduct_submit">
+          <Button type="submit">Edit Product</Button>
+        </div>
+      </form>
     </Layout>
   );
 };
