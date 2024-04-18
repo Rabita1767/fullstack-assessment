d880911968bfc774ce1954dcd1f26211c434cb0c
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2024-04-18 16:52:52 +0600

    Added logic for bad user input for rent

--- server/src/resolvers/rent.ts
+++ server/src/resolvers/rent.ts
@@ -1,3 +1,4 @@
+import { GraphQLError } from "graphql";
 import { prisma } from "../config/prisma";
 
 class _Rent_ {
@@ -10,6 +11,44 @@ class _Rent_ {
             to: string;
         }
     ) {
+        if (new Date(args.from) >= new Date(args.to)) {
+            throw new GraphQLError(`Product rental dates are not valid`!, {
+                extensions: {
+                    code: "BAD_USER_INPUT",
+                    argumentName: "id",
+                },
+            });
+        }
+
+        const existing = await prisma.rent_Instance.findFirst({
+            where: {
+                productId: args.productId,
+                from: {
+                    gte: new Date(args.from),
+                },
+                to: {
+                    lte: new Date(args.to),
+                },
+            },
+            include: {
+                product: true,
+                user: true,
+            },
+        });
+
+        // console.log(existing);
+        if (existing) {
+            throw new GraphQLError(
+                `Product is already rented between ${new Date(args.from)} and ${new Date(args.to)}`!,
+                {
+                    extensions: {
+                        code: "BAD_USER_INPUT",
+                        argumentName: "id",
+                    },
+                }
+            );
+        }
+
         const newRent = await prisma.rent_Instance.create({
             data: {
                 productId: args.productId,
